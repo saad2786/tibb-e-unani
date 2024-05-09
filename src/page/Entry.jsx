7db23bf0f5b7278de5fdc4efb87e486017e37c00
saad2786 <patilsaad2786@gmail.com>
2024-05-09 10:58:40 +0530

    Edited

--- src/page/Entry.jsx
+++ src/page/Entry.jsx
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 import { useNavigate, useParams } from "react-router-dom";
 import { fetchRecords } from "../services/apiRecords";
 import Loader from "../ui/Loader";
-import { format } from "date-fns";
+import { differenceInDays, format } from "date-fns";
 const Entry = () => {
   const [entry, setEntry] = useState({});
   const [mediciens, setMediciens] = useState([]);
@@ -17,7 +17,6 @@ const Entry = () => {
     queryKey: ["records"],
     queryFn: fetchRecords,
     onSuccess: (data) => {
-      console.log(data);
       const selectedEntry = data?.records.find(
         (record) => record.rid == entryId,
       );
@@ -29,16 +28,19 @@ const Entry = () => {
     },
     onSettled: () => {},
   });
+  const dayAgo = entry
+    ? Math.abs(differenceInDays(new Date(entry.date), new Date()))
+    : null;
 
   if (isLoading || isRefetching) return <Loader />;
   return (
     <div className="mt-16 flex flex-col items-center justify-center px-6 py-4 font-outfit">
-      <div className="w-full rounded-xl  bg-slate-300 p-4  shadow-lg ">
+      <div className="relative w-full  rounded-xl bg-slate-300  p-4 pt-6 shadow-lg ">
         <div className="px-2 py-1">
           <p className="text-sm font-light">Medicien:</p>
           {mediciens.map((medicien) => {
             return (
-              <div className="flex w-full items-center justify-between">
+              <div className="flex w-full items-center justify-between border-b border-slate-400 py-1">
                 <p className="text-lg font-semibold">{medicien.name}</p>
                 <p className="text-lg font-semibold">{medicien.qty}</p>
               </div>
@@ -66,8 +68,10 @@ const Entry = () => {
               {entry.date ? format(new Date(entry.date), "dd-MMM-yyyy") : ""}
             </p>
           </div>
-          <div className="flex-1 px-2 py-1">
-            <p className="text-lg font-semibold">14 days ago</p>
+          <div className="absolute right-4 top-3 flex-1 rounded-md border-2 border-green-600 bg-green-300 px-2 py-1 text-sm text-green-800">
+            <p className="font-normal">
+              {dayAgo ? `${dayAgo} days ago` : "Today"}
+            </p>
           </div>
         </div>
       </div>
--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,6 +1,10 @@
+import { differenceInDays } from "date-fns";
 import React from "react";
 
 const Card = ({ record }) => {
+  const dayAgo = record
+    ? Math.abs(differenceInDays(new Date(record.date), new Date()))
+    : null;
   return (
     <div className="flex w-full items-start  justify-between rounded-2xl border-2 border-slate-800 bg-green-200 px-4 py-2">
       <div>
@@ -9,7 +13,9 @@ const Card = ({ record }) => {
           {record?.growth ? "Improved" : "Not Improved"}
         </p>
       </div>
-      <p className="text-base font-normal">15 days ago</p>
+      <p className="text-base font-normal">
+        {dayAgo ? `${dayAgo} days ago` : "Today"}
+      </p>
     </div>
   );
 };
